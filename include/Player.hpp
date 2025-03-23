22c094203543744332b8703ac7618504190ba26e
Keefe Feng <kfeng62@uwo.ca>
2025-03-23 21:19:10 +0000

    A bit of an oopsie, my bad

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,21 +1,57 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Collider.hpp"
+#include "Bullet.hpp"
 #include <iostream>
 
 using namespace sf;
 
-class Tile
+class Player
 {
-public:
-    Tile(Texture *texture, Vector2f size, Vector2f position, int t);
-    ~Tile();
-
-    void render(RenderWindow &window);
-    Collider &getCollider() { return collider; };
-
 private:
-    int type; // 0 = Normal platform type, 1 = Ground, 2 = No collision
+    Vector2f position;
+    Vector2f direction;
+    Vector2f velocity;
+    float speed;
+    float jumpPower;
+    float timeLastFired;
+    int health; // 100 Default
+    bool isOnGround;
+    Texture texture;
     RectangleShape body;
     Collider collider;
+    Bullet bullet;
+
+    // Input bools
+    bool left;
+    bool right;
+    bool space;
+    bool isFiring;
+    bool canFire;
+    bool toggleMode;
+
+    Time dt; // DeltaTime
+    Clock deltaClock;
+    Clock inGameClock; // Total elapsed time in-game
+
+public:
+    Player();
+    // Is run every frame, checks for user input and fires function corresponding to input
+    void handleInput(Keyboard::Scancode key, bool checkPressed, RenderWindow &window);
+    // Gravity sim, run every physics update
+    void applyGravity(float detlaTime);
+    void move(float deltaTime);
+    // Fixed force
+    void jump();
+    void fire(RenderWindow &window);
+    void takeDamage(int amount);
+    void update();
+    void render(RenderWindow &window);
+    void renderBullet(RenderWindow &window);
+    Bullet getBullet() { return bullet; };
+
+    Collider &getCollider() { return collider; }
+    void onCollision(Vector2f direction);
+
+    bool getMode() { return toggleMode; };
 };
